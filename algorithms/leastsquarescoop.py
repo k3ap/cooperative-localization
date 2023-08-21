0bc58ed5c0b2161e18015393e806983ceaaf8e6d
Patrik Žnidaršič <posta.patrik.2@gmail.com>
2023-08-21 13:36:37 +0200

    Add derivative to least squares coop

--- algorithms/leastsquarescoop.py
+++ algorithms/leastsquarescoop.py
@@ -42,8 +42,27 @@ def solve(points, args):
                 s += sqr(edge.dist - np.linalg.norm(positions[pt]-positions[edge._dest]))
         return s
 
-    x0 = np.zeros((points[0].dim * num_agents,))
-    locs = scipy.optimize.minimize(func, x0).x
+    def grad(locs):
+        dim = network.points[0].dim
+        s = np.zeros((dim * num_agents,))
+        positions = get_positions(locs, dim)
+        idx = 0
+        for pt in network.points:
+            if pt.typ == "S":
+                continue
+
+            for edge in pt.edges.values():
+                n = np.linalg.norm(positions[pt] - positions[edge._dest])
+                for k in range(dim):
+                    s[idx+k] += (edge.dist - n) * (positions[pt][k,0] - positions[edge._dest][k,0]) / n
+
+            idx += dim
+
+        return -2 * s
+
+
+    x0 = np.random.rand(points[0].dim * num_agents)
+    locs = scipy.optimize.minimize(func, x0, jac=grad).x
 
     positions = get_positions(locs, network.points[0].dim)
     return [tuple(float(x) for x in positions[pt]) for pt in network.points]
