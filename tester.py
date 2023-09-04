8123b6ea0788b60b3abb3b13f54ea6e62eb234b5
Patrik Žnidaršič <posta.patrik.2@gmail.com>
2023-09-04 10:14:50 +0200

    Add option to set different visibilities to tester

--- tester.py
+++ tester.py
@@ -19,7 +19,7 @@ from network import DisconnectedGraphError
 from samples.standard.generate_random import generate_points
 
 
-def test_configuration(algorithm_name, point_filename, sigma, sigma_angles, args):
+def test_configuration(algorithm_name, point_filename, sigma, sigma_angles, visibility, args):
     """Test a particular configuration."""
 
     algo_name, *params = algorithm_name.split(":")
@@ -30,6 +30,7 @@ def test_configuration(algorithm_name, point_filename, sigma, sigma_angles, args
 
     args.sigma = sigma
     args.sigma_angles = sigma_angles
+    args.visibility = visibility
 
     algo_func = importlib.import_module(f"algorithms.{algo_name}").solve
 
@@ -39,7 +40,7 @@ def test_configuration(algorithm_name, point_filename, sigma, sigma_angles, args
     runs = 0
 
     for runnum in range(args.repeats):
-        print(f"Running `{algorithm_name}` on `{point_filename}` with sigmas={sigma},{sigma_angles}. Run {runnum+1}/{args.repeats}")
+        print(f"Running `{algorithm_name}` on `{point_filename}` with sigmas={sigma},{sigma_angles} and v={visibility}. Run {runnum+1}/{args.repeats}")
 
         run_error = 0
 
@@ -115,10 +116,9 @@ if __name__ == "__main__":
     )
 
     pars.add_argument(
-        "-v", "--visibility",
-        help="The maximum visible distance",
-        required=True,
-        type=float
+        "-v", "--visibilities",
+        help="The maximum visible distances to test.",
+        required=True
     )
 
     args = pars.parse_args()
@@ -127,20 +127,22 @@ if __name__ == "__main__":
     filenames = args.files.split(",")
     sigmas = list(map(float, args.sigmas.split(",")))
     sigmas_angles = list(map(float, args.sigmas_angles.split(",")))
+    visibilites = list(map(float, args.visibilities.split(",")))
 
     start = datetime.datetime.now()
 
     with open("results.csv", "w") as f:
 
         writer = csv.writer(f)
-        writer.writerow(["sample", "algorithm", "sigma", "sigma_angle", "RMSE", "time"])
+        writer.writerow(["sample", "algorithm", "sigma", "sigma_angle", "visibility", "RMSE", "time"])
 
         for fname in filenames:
             for algo in algorithms:
                 for sigma in sigmas:
                     for sigma_angle in sigmas_angles:
-                        results = test_configuration(algo, fname, sigma, sigma_angle, args)
-                        writer.writerow([fname, algo, str(sigma), str(sigma_angle)] + results)
+                        for visibility in visibilites:
+                            results = test_configuration(algo, fname, sigma, sigma_angle, visibility, args)
+                            writer.writerow([fname, algo, str(sigma), str(sigma_angle), str(visibility)] + results)
 
     end = datetime.datetime.now()
     print(f"Simulations concluded. Running time: {str(end-start)}.")
